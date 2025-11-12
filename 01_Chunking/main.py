c7744fe37b7c68a238bceaec110b5bf74db5e276
LouisZCode <lgzg90@hotmail.com>
2025-11-12 17:19:13 +0100

    stepstone: finish chunking understanding, moving to embeddings

--- 01_Chunking/main.py
+++ 01_Chunking/main.py
@@ -1,5 +1,29 @@
 from langchain_community.document_loaders import PyMuPDFLoader
 
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
 loader = PyMuPDFLoader("data/Nestle_HR_Policy.pdf")
-document = loader.load()
-print(document)
+policy = loader.load()
+#print(policy)
+
+#Text split and get the Chunks:
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000,
+    chunk_overlap=200,
+    separators=["\n\n", "\n", " ", ""]
+)
+
+chunks = text_splitter.split_documents(policy)
+
+
+# Quick and effective chunk view
+for i, chunk in enumerate(chunks[:5]): #first five
+    print(len(chunks))
+    print(f"\n--- Chunk {i+1} ---")
+    print(f"Source: {chunk.metadata['source']}")
+    print(f"Preview: {chunk.page_content[:200]}...")
+    print(f"Length: {len(chunk.page_content)} chars")
+
