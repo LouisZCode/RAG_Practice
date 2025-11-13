d08596c41f0f07434ca1e9fd68dbe166f1305145
LouisZCode <lgzg90@hotmail.com>
2025-11-13 11:47:46 +0100

    stepstone: creation of the first retriever and selected chunks from query

--- 03_Retriever/main.py
+++ 03_Retriever/main.py
@@ -28,5 +28,21 @@ chunks = text_splitter.split_documents(policy)
 
 embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
+""" 
+This was used before to create the local vector store, now we need to only load it
 vector_store = FAISS.from_documents(chunks, embeddings)
-vector_store.save_local("HR_Policy_Vector")
+vector_store.save_local("HR_Policy_Vector") """
+
+vector_store = FAISS.load_local("HR_Policy_Vector", embeddings, allow_dangerous_deserialization=True)
+
+# Create a retriever
+retriever = vector_store.as_retriever()
+
+query = "What does my HR Policy speak about? and what company does it apply to?"
+
+results = retriever.invoke(query)
+
+for i, doc in enumerate(results):
+    print(f"\n--- Result {i+1} ---")
+    print(f"Content: {doc.page_content[:200]}...")
+    print(f"Source: {doc.metadata['source']}")
