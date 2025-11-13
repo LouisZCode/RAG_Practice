f2ab385395f423d2c1ea6d595f72478dca82cd3b
LouisZCode <lgzg90@hotmail.com>
2025-11-13 12:56:14 +0100

    feature: add the tool to the agent, and was able to retrieve specific information!

--- 04_Agent_with_RAG/main.py
+++ 04_Agent_with_RAG/main.py
@@ -6,9 +6,13 @@ from dotenv import load_dotenv
 
 from langchain_community.vectorstores import FAISS
 
+
+from langchain.agents import create_agent
+from langchain_core.tools import create_retriever_tool
+
 load_dotenv()
 
-loader = PyMuPDFLoader(
+""" loader = PyMuPDFLoader(
     "data/Nestle_HR_Policy.pdf"
 )
 
@@ -20,29 +24,52 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_overlap = 200
 )
 
-chunks = text_splitter.split_documents(policy)
+chunks = text_splitter.split_documents(policy) """
 
 """ for i, chunk in enumerate(chunks):
     print(chunk) """
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
-
 """ #This was used before to create the local vector store, now we need to only load it
+
 vector_store = FAISS.from_documents(chunks, embeddings)
 vector_store.save_local("HR_Policy_Vector") """
 
-vector_store = FAISS.load_local("HR_Policy_Vector", embeddings, allow_dangerous_deserialization=True)
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 
+vector_store = FAISS.load_local("HR_Policy_Vector", embeddings, allow_dangerous_deserialization=True)
 # Create a retriever
 retriever = vector_store.as_retriever(
     search_kwargs={"k": 3}
 )
 
-query = "What does my HR Policy speak about? and what company does it apply to?"
 
-results = retriever.invoke(query)
+#This is new:
+retriever_tool = create_retriever_tool(
+    retriever,
+    name="search_documents",
+    description="Search through the document knowledge base to find relevant information."
+)
 
-for i, doc in enumerate(results):
-    print(f"\n--- Result {i+1} ---")
-    print(f"Content: {doc.page_content[:200]}...")
-    print(f"Source: {doc.metadata['source']}")
+
+System_Prompt = """
+You are a internal model for the company.
+You alway use your retrieval_tool to find context before answering.
+You answer honestly, and if the context does not provide an answer, you let the user know."""
+
+
+agent = create_agent(
+    model="openai:gpt-5-mini",
+    system_prompt=System_Prompt,
+    tools=[retriever_tool]
+)
+
+
+query = input("Ask something to the HR Policy:\n")
+
+result = agent.invoke({
+    "role" : "user",
+    "messages" : query
+    })
+
+for i,msg in enumerate(result["messages"]):
+    msg.pretty_print()
